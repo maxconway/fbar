8496d8535b83b533e52f4711ecdb2b4ea511cd14
Max Conway <max.conway@cl.cam.ac.uk>
2016-01-12 17:09:24 +0000

    New utilities

--- R/compartment_utilities.R
+++ R/compartment_utilities.R
@@ -11,6 +11,23 @@ list_compartments <- function(model){
     unique
 }
 
+list_metabolites <- function(model){
+  model$equation %>%
+    str_split('[[:space:]]*[+<=>]+[[:space:]]*') %>%
+    flatten %>%
+    str_replace('\\[\\w+]$', '') %>%
+    str_replace('^[0-9]+(\\.[0-9]+)? ', '') %>%
+    unique
+}
+
+list_metabolites_withcompartment <- function(model){
+  model$equation %>%
+    str_split('[[:space:]]*[+<=>]+[[:space:]]*') %>%
+    flatten %>%
+    str_replace('^[0-9]+(\\.[0-9]+)? ', '') %>%
+    unique
+}
+
 alter_compartment <- function(model, from, to){
   assert_that(length(from)==1,length(to)==1)
   mutate(model, equation = equation %>%
@@ -18,11 +35,11 @@ alter_compartment <- function(model, from, to){
          )
 }
 
-alter_comparments <- function(model, from, to){
+alter_compartments <- function(model, from, to){
   assert_that(length(from)==length(to) | length(from)==1 | length(to)==1)
   alterations <- data.frame(from, to, stringsAsFactors = FALSE)
   for(i in 1:nrow(alterations)){
-    model <- alter_compartments(model, from = alterations[i,'from'], to=alterations[i,'to'])
+    model <- alter_compartment(model, from = alterations[i,'from'], to=alterations[i,'to'])
   }
   return(model)
 }
@@ -30,5 +47,7 @@ alter_comparments <- function(model, from, to){
 defactor_compartments <- function(model, name){
   compartments <- list_compartments(model)
   newcompartments <- paste0(name,'_',compartments)
-  alter_comparments(model, from=compartments, to=newcompartments)
+  alter_compartments(model, from=compartments, to=newcompartments) %>%
+    mutate(abbreviation = paste(name, abbreviation))
 }
+
