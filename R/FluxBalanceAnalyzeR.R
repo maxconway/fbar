8573715fca8d43d5087559ba9159c5add6bd2361
Max Conway <max.conway@cl.cam.ac.uk>
2016-12-06 16:16:37 +0000

    More documentation

---
+++ R/FluxBalanceAnalyzeR.R
@@ -0,0 +1,14 @@
+#' FluxBalanceAnalyzeR: Flux Balance Analysis with a tidy data approach
+#' 
+#' FluxBalanceAnalyzeR is a simple, easy to use Flux Balance Analysis package with a tidy data approach. 
+#' Just \code{data_frames} and the occasional \code{list}, no new classes to learn. 
+#' The focus is on simplicity and speed. 
+#' Models are expected as a flat table, and results can be simply appended to the table. 
+#' This makes this package very suitable for useage in pipelines with pre- and post- processing of models and results, so that it works well as a backbone for customized methods. 
+#' Loading, parsing and evaluating a model takes around 0.1s, which, together with the straightforward data structures used, makes this library very suitable for large parameter sweeps.
+#' 
+#' For a list of functions in the package, see \code{vignette('functions','FluxBalanceAnalyzeR')}
+#' 
+#' @docType package
+#' @name FluxBalanceAnalyzeR
+NULL
