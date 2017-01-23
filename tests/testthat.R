c907de38b3936e4d9707881cea008ae4769d49d0
maxconway <conway.max1@gmail.com>
2017-01-23 00:32:30 +0000

    Now loads available solvers before running tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,4 +2,7 @@ suppressMessages(library(testthat))
 suppressMessages(library(fbar))
 suppressMessages(library(tidyverse))
 
+suppressMessages(walk(ROI::ROI_available_solvers(), requireNamespace))
+  
+
 test_check("fbar")
