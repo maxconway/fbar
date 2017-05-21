e4333c0702618e81cf0f2848e680451a40bc8690
Max Conway <conway.max1@gmail.com>
2017-05-21 19:55:25 +0100

    altered tests to make sure plugin installed

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,7 @@
 suppressMessages(library(testthat))
 suppressMessages(library(fbar))
 suppressMessages(library(tidyverse))
+suppressMessages(library(ROI.plugin.ecos))
 
 suppressMessages(walk(ROI::ROI_available_solvers(), requireNamespace))
   
