1d7946cf05056ce897a5688fdefaeebd5e4296df
Max Conway <max.conway@cl.cam.ac.uk>
2017-12-19 16:33:51 +0000

    Updates to documentation to pass checks correctly

--- R/data.R
+++ R/data.R
@@ -3,14 +3,15 @@
 #' @format A data frame with 95 rows and 7 columns:
 #' \describe{
 #'   \item{abbreviation}{an abbreviated reaction name, acts as the reaction id}
-#'   \item{officialName}{full reaction name}
-#'   \item{equation}{reaction equation}
 #'   \item{lowbnd}{lower bound on the reaction rate}
 #'   \item{uppbnd}{upper bound on the reaction rate}
 #'   \item{obj_coef}{identifies a reaction (or reactions) for which the maximum possible rate should be found}
+#'   \item{equation}{reaction equation}
+#'   \item{officialName}{full reaction name}
 #'   \item{geneAssociation}{A boolean combination of genes which control the reaction}
+#'   \item{subsystem}{an indicator of reaction function}
 #' }
-#' @source \url{http://gcrg.ucsd.edu/Downloads/EcoliCore}, \href{http://www.asmscience.org/content/journal/ecosalplus/10.1128/ecosalplus.10.2.1}{Reconstruction and Use of Microbial Metabolic Networks: the Core Escherichia coli Metabolic Model as an Educational Guide}, \href{https://www.ncbi.nlm.nih.gov/pubmed/21988831}{A comprehensive genome-scale reconstruction of Escherichia coli metabolism--2011.}
+#' @source \url{http://bigg.ucsd.edu}, \url{http://gcrg.ucsd.edu/Downloads/EcoliCore}, \href{http://www.asmscience.org/content/journal/ecosalplus/10.1128/ecosalplus.10.2.1}{Reconstruction and Use of Microbial Metabolic Networks: the Core Escherichia coli Metabolic Model as an Educational Guide}, \href{https://www.ncbi.nlm.nih.gov/pubmed/21988831}{A comprehensive genome-scale reconstruction of Escherichia coli metabolism--2011.}
 'ecoli_core'
 
 #' A full size E. coli model.
@@ -18,15 +19,21 @@
 #' @format A data frame with 2,583 rows and 10 columns:
 #' \describe{
 #'   \item{abbreviation}{an abbreviated reaction name, acts as the reaction id}
-#'   \item{name}{full reaction name}
-#'   \item{equation}{reaction equation}
-#'   \item{subsystem}{an indicator of reaction function}
 #'   \item{lowbnd}{lower bound on the reaction rate}
 #'   \item{uppbnd}{upper bound on the reaction rate}
 #'   \item{obj_coef}{identifies a reaction (or reactions) for which the maximum possible rate should be found}
-#'   \item{GPRassoc}{Boolean combination of genes and proteins which control the reaction}
-#'   \item{GRassoc}{Boolean combination of genes which control the reaction}
-#'   \item{PRassoc}{Boolean combination of proteins which control the reaction}
+#'   \item{equation}{reaction equation}
+#'   \item{officialName}{full reaction name}
+#'   \item{geneAssociation}{A boolean combination of genes which control the reaction}
+#'   \item{subsystem}{an indicator of reaction function}
 #' }
-#' @source \href{https://www.ncbi.nlm.nih.gov/pubmed/21988831}{A comprehensive genome-scale reconstruction of Escherichia coli metabolism--2011.}
+#' @source \url{http://bigg.ucsd.edu}, \href{https://www.ncbi.nlm.nih.gov/pubmed/21988831}{A comprehensive genome-scale reconstruction of Escherichia coli metabolism--2011.}
 'iJO1366'
+
+#' A subset of exchange reactions annotated to indicate typical availability
+#' @format A data frame with 25 rows and 2 columns:
+#' \describe{
+#'   \item{abbreviation}{an exchange reaction id}
+#'   \item{nutrient_type}{the nutrient availability, one of 'micro', 'macro' or 'substrate'}
+#' }
+'nutrient_types'
